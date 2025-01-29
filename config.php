f751aeb7542d8157c683e5ab3214f998ebf2ff5f
Salman Saad <devsalmansaad@gmail.com>
2025-01-29 23:50:49 +0600

    init

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
     $dbserver="localhost"; // Database Server or Host
     $dbuser="root"; // Database User
     $dbpass=""; // Database User's Password
-    $db="lab_foodhub-secur"; // Database Name
+    $db="foodhub-secure"; // Database Name
     $dbport="3306"; // Database Port
 ?>
